a13eb608cb0e43b46822826a00649a117ffa4775
erikvim <erikdavid.hedman21@outlook.com>
2025-03-11 22:46:53 -0400

    Created a simple project to return the status code of a HttpGet response

---
+++ ch5/statusCodeProperty/go.mod
@@ -0,0 +1,3 @@
+module github.com/Http/tree/main/ch5/statusCodeProperty
+
+go 1.24.0
---
+++ ch5/statusCodeProperty/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+)
+
+func getUserCode(url string) int {
+	res, err := http.Get(url)
+	if err != nil {
+		return 0
+	}
+	defer res.Body.Close()
+
+	status := res.StatusCode
+
+	return status
+}
