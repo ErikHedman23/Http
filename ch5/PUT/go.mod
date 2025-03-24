6b84638ddb27fc7bbb63b3b7e121930a6cf80b07
erikvim <erikdavid.hedman21@outlook.com>
2025-03-23 21:11:49 -0400

    Created a basic project to learn HttpPut method in Go

---
+++ ch5/PUT/go.mod
@@ -0,0 +1,3 @@
+module github.com/ErikHedman/Http/ch5/PUT
+
+go 1.24.0
---
+++ ch5/PUT/http.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+)
+
+func updateUser(baseURL, id, apiKey string, data User) (User, error) {
+	fullURL := baseURL + "/" + id
+	marshalledUser, err := json.Marshal(data)
+	if err != nil {
+		return User{}, err
+	}
+
+	req, err := http.NewRequest("PUT", fullURL, bytes.NewBuffer(marshalledUser))
+	if err != nil {
+		return User{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-API-Key", "apiKey")
+
+	client := &http.Client{}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+
+	var updatedUser User
+	decoder := json.NewDecoder(resp.Body)
+	if err := decoder.Decode(&updatedUser); err != nil {
+		return User{}, err
+	}
+
+	return updatedUser, nil
+
+}
+
+func getUserById(baseURL, id, apiKey string) (User, error) {
+	fullURL := baseURL + "/" + id
+
+	req, err := http.NewRequest("GET", fullURL, nil)
+	if err != nil {
+		return User{}, err
+	}
+
+	req.Header.Set("X-API-Key", apiKey)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return User{}, err
+	}
+
+	var retrievedUser User
+	decoder := json.NewDecoder(resp.Body)
+
+	if err := decoder.Decode(&retrievedUser); err != nil {
+		return User{}, nil
+	}
+
+	return retrievedUser, nil
+
+}
